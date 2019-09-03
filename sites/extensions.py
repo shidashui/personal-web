9e2b4dff20bacd40740f8ab8bdc03ff56af07093
shui <164635470@qq.com>
2019-09-03 23:03:25 +0800

    extensions

---
+++ sites/extensions.py
@@ -0,0 +1,43 @@
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, AnonymousUserMixin
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+mail = Mail()
+moment = Moment()
+dropzone = Dropzone()
+csrf = CSRFProtect()
+avatars = Avatars()
+whooshee = Whooshee()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from sites.models.user import User
+    user = User.query.get(int(user_id))
+    return user
+
+login_manager.login_view = 'auth.login'
+# login_manager.login_message = '请登录'
+login_manager.login_message_category = 'warning'
+
+#对话不新鲜跳转的端点
+login_manager.refresh_view = 'auth.re_authenticate'
+login_manager.needs_refresh_message = u'为了保护你的账户安全，请重新登录。'
+login_manager.needs_refresh_message_category = 'warning'
+
+
+#匿名用户的权限管理
+class Guest(AnonymousUserMixin):
+    @property
+    def is_admin(selfs):
+        return False
+
+    def can(self, permission_name):
+        return False
+
+login_manager.anonymous_user = Guest
---
+++ sites/models/user.py
@@ -0,0 +1,143 @@
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, index=True)
+    email = db.Column(db.String(254), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+    name = db.Column(db.String(30))
+    website = db.Column(db.String(255))
+    bio = db.Column(db.String(120))
+    location = db.Column(db.String(50))
+    member_since = db.Column(db.DateTime, default=datetime.utcnow)
+    avatar_s = db.Column(db.String(64))
+    avatar_m = db.Column(db.String(64))
+    avatar_l = db.Column(db.String(64))
+
+    avatar_raw = db.Column(db.String(64)) #用户上传的头像
+
+    #消息开关
+    receive_comment_notification = db.Column(db.Boolean, default=True)
+    receive_follow_notification = db.Column(db.Boolean, default=True)
+    receive_collect_notification = db.Column(db.Boolean, default=True)
+
+    #隐私设置
+    public_collections = db.Column(db.Boolean, default=True)
+
+    confirmed = db.Column(db.Boolean, default=False) #是否邮箱验证
+
+    locked = db.Column(db.Boolean, default=False)   #用户锁定
+    active = db.Column(db.Boolean, default=True)    #封禁用户
+
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+    role = db.relationship('Role', back_populates='users')
+    photos = db.relationship('Photo', back_populates='author', cascade='all') #级联设为all，用户被删除，相应图片全删除
+    comments = db.relationship('Comment', back_populates='author', cascade='all')
+    collections = db.relationship('Collect', back_populates='collector', cascade='all')
+
+    following = db.relationship('Follow', foreign_keys=[Follow.follower_id], back_populates='follower',
+                                lazy='dynamic', cascade='all')
+    followers = db.relationship('Follow', foreign_keys=[Follow.followed_id], back_populates='followed',
+                                lazy='dynamic', cascade='all')
+
+    notifications = db.relationship('Notification', back_populates='receiver', cascade='all')
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        self.generate_avatar()
+        self.follow(self) #follow self
+        self.set_role()
+
+    def generate_avatar(self):
+        avatar = Identicon()
+        filenames = avatar.generate(text=self.username)
+        self.avatar_s = filenames[0]
+        self.avatar_m = filenames[1]
+        self.avatar_l = filenames[2]
+        db.session.commit()
+
+    def set_role(self):
+        if self.role is None:
+            if self.email == current_app.config['ALBUMY_ADMIN_EMAIL']:
+                self.role = Role.query.filter_by(name='Administrator').first()
+            else:
+                self.role = Role.query.filter_by(name='User').first()
+            db.session.commit()
+
+    @property
+    def is_admin(self):
+        return self.role.name == 'Administrator'
+
+    def can(self, permission_name):
+        permission = Permission.query.filter_by(name=permission_name).first()
+        return permission is not None and self.role is not None and permission in self.role.permissions
+
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+    #收藏和取消收藏
+    def collect(self, photo):
+        if not self.is_collecting(photo):
+            collect = Collect(collector=self, collected=photo)
+            db.session.add(collect)
+            db.session.commit()
+
+    def uncollect(self, photo):
+        collect = Collect.query.with_parent(self).filter_by(collected_id=photo.id).first()
+        if collect:
+            db.session.delete(collect)
+            db.session.commit()
+
+    def is_collecting(self, photo):
+        return Collect.query.with_parent(self).filter_by(collected_id=photo.id).first() is not None
+
+    #关注和取消关注
+    def follow(self, user):
+        if not self.is_following(user):
+            follow = Follow(follower=self, followed=user)
+            db.session.add(follow)
+            db.session.commit()
+
+    def unfollow(self, user):
+        follow = self.following.filter_by(followed_id=user.id).first()
+        if follow:
+            db.session.delete(follow)
+            db.session.commit()
+
+    def is_following(self, user):
+        if user.id is None: #在实例化的时候，还未提交数据库，user.id为none,为了避免构造方法报错，就直接返回False
+            return False
+        return self.following.filter_by(followed_id=user.id).first() is not None
+
+    def is_followed_by(self, user):
+        return self.followers.filter_by(follower_id=user.id).first() is not None
+
+    def followed_photos(self):
+        return Photo.query.join(Follow, Follow.followed_id == Photo.author_id).filter(Follow.follower_id == self.id)
+
+    #用户锁定
+    def lock(self):
+        self.locked = True
+        self.role = Role.query.filter_by(name='Locked').first()
+        db.session.commit()
+
+    def unlock(self):
+        self.locked = False
+        self.role = Role.query.filter_by(name='User').first()
+        db.session.commit()
+
+    #用户封禁
+    @property
+    def is_active(self):
+        return self.active
+
+    def block(self):
+        self.active = False
+        db.session.commit()
+
+    def unblock(self):
+        self.active = True
+        db.session.commit()
